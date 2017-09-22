2e6b56a3ce7f9961ab2880ece52334ae35a7c6ac
Janne Paalijarvi <jpaalija@gmail.com>
2017-09-22 15:08:31 +0300

    Added comment

--- scripts/dhcp/sbin/firewall_shell.sh
+++ scripts/dhcp/sbin/firewall_shell.sh
@@ -10,6 +10,7 @@ reset_fw_rules_by_tag $FIREWALL_TAG_SHELL
 
 # Allow shell
 #/sbin/iptables -A INPUT -i $IF_SHELL -m state --state NEW,RELATED,ESTABLISHED -j ACCEPT
+# Allow receiving messages to for example locally bound interface:
 /sbin/iptables -A INPUT -i $IF_SHELL -d $ADDR_PUB_SHELL -m state --state RELATED,ESTABLISHED -j ACCEPT -m comment --comment "$FIREWALL_TAG_SHELL"
 /sbin/iptables -t nat -A PREROUTING -i $IF_SHELL -p tcp -d $ADDR_PUB_SHELL --dport 1:65535 -j DNAT --to-destination $ADDR_PRIV_SHELL:22 -m comment --comment "$FIREWALL_TAG_SHELL"
 /sbin/iptables -t nat -A PREROUTING -p tcp -s $RANGE_LAN -d $ADDR_PUB_SHELL --dport 1:65535 -j DNAT --to-destination $ADDR_PRIV_SHELL:22 -m comment --comment "$FIREWALL_TAG_SHELL"
