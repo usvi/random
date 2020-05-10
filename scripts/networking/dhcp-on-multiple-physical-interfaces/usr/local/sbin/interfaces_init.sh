a41eff1f568609b109bc5529b7f6c47145c2ba98
Janne Paalijarvi <jpaalija@gmail.com>
2020-05-11 00:44:07 +0300

    Unifying physical to match virtual

---
+++ scripts/networking/dhcp-on-multiple-physical-interfaces/usr/local/sbin/interfaces_init.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# This script is run from rc.local (and only once)
+
+. /usr/local/sbin/networking_defs.sh
+
+rmdir $SCRIPTS_LOCKDIR
+
+# No other need to specificly handle interfaces,
+# /etc/network/interfaces works somewhat in this.
