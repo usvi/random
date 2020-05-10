cbbeda2dc4e06a607af3d6b8b39fed0def5b9a7e
Janne Paalijarvi <jpaalija@gmail.com>
2020-05-10 23:41:17 +0300

    Sanitazing scripts

--- scripts/networking/dhcp-on-multiple-virtual-interfaces/usr/local/sbin/firewall_init.sh
+++ scripts/networking/dhcp-on-multiple-virtual-interfaces/usr/local/sbin/firewall_init.sh
@@ -4,16 +4,6 @@
 
 . /usr/local/sbin/networking_defs.sh
 
-rmdir $SCRIPTS_LOCKDIR
-
-# Create the virtual interfaces
-ifconfig $IF_VIRTUAL_BASE up promisc
-ip link add link $IF_VIRTUAL_BASE address 00:90:0b:ff:10:5b $IF_PUB0 type macvlan
-ip link set $IF_PUB0 up
-ip link add link $IF_VIRTUAL_BASE address 00:90:0b:ff:11:5b $IF_PUB1 type macvlan
-ip link set $IF_PUB1 up
-ip link add link $IF_VIRTUAL_BASE address 00:90:0b:ff:12:5b $IF_PUB2 type macvlan
-ip link set $IF_PUB2 up
 
 #Flush chains
 /sbin/iptables -F
