6cdbf2979fd7fb6a3bcef559f2caab589bc775b3
Janne Paalijarvi <jpaalija@gmail.com>
2020-05-10 22:48:55 +0300

    Debugging hairpinning issue

--- scripts/networking/dhcp-on-multiple-virtual-interfaces/usr/local/sbin/setup_vpn.sh
+++ scripts/networking/dhcp-on-multiple-virtual-interfaces/usr/local/sbin/setup_vpn.sh
@@ -2,7 +2,7 @@
 
 . /usr/local/sbin/networking_defs.sh
 
-ADDR_PUB_VPN=`/sbin/ifconfig IF_PUB0 | grep 'inet addr:' | sed s/.*'inet addr:'// | sed s/' '.*//`
+ADDR_PUB_VPN=`/sbin/ifconfig $IF_PUB0 | grep 'inet addr:' | sed s/.*'inet addr:'// | sed s/' '.*//`
 OPENVPN_CONF_LISTEN="/etc/openvpn/listen.conf"
 
 echo "local $ADDR_PUB_VPN" > $OPENVPN_CONF_LISTEN
