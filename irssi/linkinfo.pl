46482af1c3ceaf94fcaa4288592c512b5a7e1754
Janne Paalijarvi <jpaalija@gmail.com>
2020-07-06 00:27:30 +0300

    Fixes more stuff. Too drunk to describe further.

--- irssi/linkinfo.pl
+++ irssi/linkinfo.pl
@@ -14,8 +14,9 @@ $ua->timeout(10);
 #$ua->agent("Wget/1.17.1 (linux-gnu)");
 $ua->agent("Omaropotti/1.0 (linux-gnu)");
 #2020-06-30: Stupid twitter fixes
+#2020-07-05: Fallback title tag parsing if built-in fails
 
-$VERSION = '0.5';
+$VERSION = '0.5.1';
 %IRSSI =
 (
  authors     => 'Mr. Janne Paalijarvi',
@@ -23,88 +24,98 @@ $VERSION = '0.5';
  name        => 'Link info printer',
  description => 'This script prints link info from channels URLs',
  license     => 'GPL',
- changed     => 'Tue 30 Jun 2020 11:30:54 PM EEST'
+ changed     => 'Mon 06 Jul 2020 12:27:12 AM EEST'
 );
 
 my $no_chans .= " #piraattipuolue/IRCnet #sivusto/PirateIRC #keski-suomi/PirateIRC #helsinki/PirateIRC #toiminta/PirateIRC #uusimaa/PirateIRC #piraattinuoret/PirateIRC #piraattipuolue/PirateIRC ";
 
 sub get_title
 {
-	my $url = $_[0];
-	my $twitter = 0;
+    my $url = $_[0];
+    my $twitter = 0;
+    
+    if (index($url, "https://twitter.com") == 0)
+    {
+	$url = substr($url, length("https://twitter.com"));
+	$url = "https://nitter.net" . $url;
+	$twitter = 1;
+    }
+    
+    my $response = $ua->head($url,
+			     'Accept' => 'text/html');
+    
+    if($response->is_success() && !($response->content_type() eq "text/html"))
+    {
+	return "";
+    }
+    $response = $ua->get($url);
+    
+    if(!($response->is_success()))
+    {
+	return "";
+    }
+    my $title = $response->title();
+    $title = decode_entities($title);
+    $title =~ s/\s+/ /g;
+    $title =~ s/^\s+|\s+$//g;
 
-	if (index($url, "https://twitter.com") == 0)
-	{
-	    $url = substr($url, length("https://twitter.com"));
-	    $url = "https://nitter.net" . $url;
-	    $twitter = 1;
-	}
+    if (length($title) == 0)
+    {
+	my $html = $response->content();
 
-	my $response = $ua->head($url,
-	    'Accept' => 'text/html');
-
-	if($response->is_success() && !($response->content_type() eq "text/html"))
-	{
-		return "";
-	}
-	$response = $ua->get($url);
-
-	if(!($response->is_success()))
-	{
-		return "";
-	}
-	my $title = $response->title();
+	($title) = $html =~ m/<\s*title[^>]*>(.+)<\s*\/\s*title/gi;
 	$title = decode_entities($title);
 	$title =~ s/\s+/ /g;
 	$title =~ s/^\s+|\s+$//g;
-	
-	if((length($title) > 0) and (length($title) < ($twitter ? 550 : 350)))
-	{
-		return "Title: " . $title;
-	}
-	else
-	{
-		return "";
-	}
+    }
+    
+    if((length($title) > 0) and (length($title) < ($twitter ? 550 : 350)))
+    {
+	return "Title: " . $title;
+    }
+    else
+    {
+	return "";
+    }
 }
 
 sub check_for_urls
 {
-	my $temp_server = $_[0];
-	my $temp_message = $_[1];
-	my $temp_nick = $_[2];
-	my $temp_channel = lc($_[4]);
+    my $temp_server = $_[0];
+    my $temp_message = $_[1];
+    my $temp_nick = $_[2];
+    my $temp_channel = lc($_[4]);
+    
+    if(!(index(" " . lc($no_chans) . " ", " " . lc($temp_channel) . "/" . lc($temp_server->{tag}) ." ") != -1))
+    {
+	#my @url_tokens = ($temp_message =~ m/([k]{0,1}http[s]{0,1}\:\/\/.*?[^( )\t]*).*?/ig);
+	my @url_tokens = ($temp_message =~ m/(http[s]{0,1}\:\/\/.*?[^( )\t]*).*?/ig);
+	my $return_string = "";
 
-	if(!(index(" " . lc($no_chans) . " ", " " . lc($temp_channel) . "/" . lc($temp_server->{tag}) ." ") != -1))
+	foreach(@url_tokens)
 	{
-		#my @url_tokens = ($temp_message =~ m/([k]{0,1}http[s]{0,1}\:\/\/.*?[^( )\t]*).*?/ig);
-		my @url_tokens = ($temp_message =~ m/(http[s]{0,1}\:\/\/.*?[^( )\t]*).*?/ig);
-		my $return_string = "";
-
-		foreach(@url_tokens)
-		{
-			if(length($_) > 3)
-			{
-				if(length($return_string) > 0)
-				{
-					$return_string .= " | ";
-				}
-				my $temp_title = get_title($_);
-
-				if(utf8::is_utf8($temp_title))
-				{
-					#$return_string .= "UTF8";
-					$temp_title =~ s/—/\-/g;
-					$temp_title = encode("iso-8859-1", $temp_title);
-				}
-
-				$return_string .= $temp_title;
-			}
-		}
+	    if(length($_) > 3)
+	    {
 		if(length($return_string) > 0)
 		{
-			($temp_server->window_find_item($temp_channel))->command("SAY " . $return_string);
+		    $return_string .= " | ";
+				}
+		my $temp_title = get_title($_);
+		
+		if(utf8::is_utf8($temp_title))
+		{
+		    #$return_string .= "UTF8";
+		    $temp_title =~ s/—/\-/g;
+					$temp_title = encode("iso-8859-1", $temp_title);
 		}
+		
+		$return_string .= $temp_title;
+	    }
+		}
+	if(length($return_string) > 0)
+	{
+	    ($temp_server->window_find_item($temp_channel))->command("SAY " . $return_string);
+	}
     }
 }
 Irssi::signal_add('message public', 'check_for_urls');
