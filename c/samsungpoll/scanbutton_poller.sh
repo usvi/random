f13a2df38d41a0e5b3b1627b3b038c4d975c23c7
Janne Paalijarvi <jpaalija@gmail.com>
2025-02-01 21:36:36 +0200

    Resets scanner usb power feed if scanning fails

--- c/samsungpoll/scanbutton_poller.sh
+++ c/samsungpoll/scanbutton_poller.sh
@@ -1,8 +1,21 @@
-#!/bin/bash
+#!/bin/sh
 #SCANNER=xerox_mfp:libusb:001:003
 SCANNER=""
 SCAN_LOCATION_BASE=/media/scans
 SCAN_SLEEP_SECS=7
+
+SCANNER_USB_ID_VENDOR="04e8"
+SCANNER_USB_ID_DEVICE="3441"
+
+USB_DEVICES_SYS_BASE="/sys/bus/usb/devices"
+USB_SYS_FILE_CONFIGURATION="bConfigurationValue"
+USB_SYS_FILE_VENDOR="idVendor"
+USB_SYS_FILE_DEVICE="idProduct"
+
+USB_SYS_SETTING_OFF=0
+USB_SYS_SETTING_ON=1
+USB_SYS_SETTING_WAIT=1
+
 
 while sleep 1;
 do
@@ -29,9 +42,35 @@ do
 	
 	scanimage -d "xerox_mfp:$SCANNER" > $FULL_FILEPREFIX.pnm
 
-	# If we failed to scan, sniff the scanner again.
+	# If we failed to scan, power off the scanner, then power back
+	# on and sniff again.
+	
 	if [ $? -ne 0 ];
 	then
+
+	    USB_SYS_PATH_CONFIGURATION=""
+
+	    for TEMP_USB_SYS_PATH in "$USB_DEVICES_SYS_BASE"/*
+	    do
+		if [ -f "$TEMP_USB_SYS_PATH/$USB_SYS_FILE_VENDOR" -a \
+			-f "$TEMP_USB_SYS_PATH/$USB_SYS_FILE_DEVICE" ]
+		then
+
+		    if [ "`cat $TEMP_USB_SYS_PATH/$USB_SYS_FILE_VENDOR`" = "$SCANNER_USB_ID_VENDOR" -a \
+			 "`cat $TEMP_USB_SYS_PATH/$USB_SYS_FILE_DEVICE`" = "$SCANNER_USB_ID_DEVICE" ]
+		    then
+			USB_SYS_PATH_CONFIGURATION="$TEMP_USB_SYS_PATH/$USB_SYS_FILE_CONFIGURATION"
+		    fi
+		fi
+	    done
+
+	    if [ -f "$USB_SYS_PATH_CONFIGURATION" ]
+	    then
+		echo "$USB_SYS_SETTING_OFF" > "$USB_SYS_PATH_CONFIGURATION"
+		sleep "$USB_SYS_SETTING_WAIT"
+		echo "$USB_SYS_SETTING_ON" > "$USB_SYS_PATH_CONFIGURATION"
+	    fi
+	    
 	    SCANNER=`sane-find-scanner -q | grep SCX | sed "s/.* at //;"`
 	    scanimage -d "xerox_mfp:$SCANNER" > $FULL_FILEPREFIX.pnm
 	fi
